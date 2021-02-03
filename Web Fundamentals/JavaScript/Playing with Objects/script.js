311de3d83e31d9e5243bc113a8649634015eacc8
RetineCocaj <retinecocaj@gmail.com>
2021-02-03 13:41:23 +0100

    Create script.js

---
+++ Web Fundamentals/JavaScript/Playing with Objects/script.js
@@ -0,0 +1,12 @@
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+
+// print/log John's age
+console.log(users[1].age);
+
+// print/log the name of the first object
+console.log(users[0].name);
+
+// print/log the name and age of each user using a for loop
+for(var index in users){
+    console.log(users[index].name + " - " + users[index].age);
+}
