57895d4bdde9c436da4d8890b89ea5635bc80055
RetineCocaj <retinecocaj@gmail.com>
2021-02-28 21:08:13 +0100

    Time Display assigment

---
+++ C# .NET Core/ASP.NET Core/TimeDisplay/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+namespace TimeDisplay
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Index(){
+            return View("Index");
+        }
+    }
+}
