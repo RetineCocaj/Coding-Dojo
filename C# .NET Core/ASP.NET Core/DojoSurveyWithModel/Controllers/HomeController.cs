b19a86b22864f9aec7c20e9428b70825b5606fdf
RetineCocaj <retinecocaj@gmail.com>
2021-03-01 13:45:36 +0100

    Dojo Survey with Model assignment

---
+++ C# .NET Core/ASP.NET Core/DojoSurveyWithModel/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index(){
+            return View("Index");
+        }
+
+        [HttpPost("result")]
+        public IActionResult Result(Survey result){
+            return View("Result", result);
+        }
+    }
+}
