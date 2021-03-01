7ac4735139c60ab70fabce736fc00ef592d09c7c
RetineCocaj <retinecocaj@gmail.com>
2021-03-01 20:38:06 +0100

    Form Submission assignment

---
+++ C# .NET Core/ASP.NET Core/FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FormSubmission.Models;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index(){
+            return View("Index");
+        }
+
+        [HttpPost("AddUser")]
+        public IActionResult AddUser(User user){
+            if(ModelState.IsValid){ return RedirectToAction("Success"); }
+            return View("Index");
+        }
+
+        [HttpGet("success")]
+        public ViewResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ C# .NET Core/ASP.NET Core/FormSubmission/Models/User.cs
@@ -0,0 +1,32 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+namespace FormSubmission.Models
+{
+    public class User
+    {
+        [Display(Name="First Name")]
+        [Required]
+        [MinLength(4, ErrorMessage="Field must be 3 characters or more")]
+        public string FirstName{ get; set; }
+
+        [Display(Name="Last Name")]
+        [Required]
+        [MinLength(4, ErrorMessage="Field must be 3 characters or more")]
+        public string LastName{ get; set; }
+
+        [Display(Name="Age")]
+        [Required]
+        [Range(0, Int32.MaxValue, ErrorMessage="Age must be a positive number")]
+        public int Age{ get; set; }
+
+        [Display(Name="Email Address")]
+        [Required]
+        [EmailAddress]
+        public string EmailAddress{ get; set; }
+
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$", 
+        ErrorMessage = "Password must be at least 8 characters, no more than 15 characters, and must include at least one upper case letter, one lower case letter, and one numeric digit.")]
+        public string Password{ get; set;}
+    }
+}
