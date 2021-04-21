5a33771e0a62ba7f6b4c70e3dc60c709dd08e2d3
Retinë Çoçaj <retine@gjirafa.com>
2021-04-21 12:12:51 +0200

    Min tofront

--- Projects & Algorithms/Arrays/MinToFront/Program.cs
+++ Projects & Algorithms/Arrays/MinToFront/Program.cs
@@ -6,7 +6,35 @@ namespace MinToFront
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = {8,5,2,4,7,1,3};
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            int[] newArr = MinToFront(arr);
+            for(int i = 0; i < newArr.Length; i++)
+            {
+                Console.Write(newArr[i] + " ");
+            }
+        }
+
+        public static int[] MinToFront(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if(min > arr[i]) min = arr[i];
+            }
+
+            int[] newArr = new int[arr.Length];
+            newArr[0] = min;
+            for (int i = 1; i < newArr.Length; i++)
+            {
+                if(arr[i-1] == min) newArr[i] = arr[i];
+                else newArr[i] = arr[i-1];
+            }
+            return newArr;
         }
     }
 }
