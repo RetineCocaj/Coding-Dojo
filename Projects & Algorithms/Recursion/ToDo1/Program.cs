86b6bb4138ad3efb35936113f10e6f1f79347186
Retinë Çoçaj <retine@gjirafa.com>
2021-04-24 17:47:18 +0200

    To do 1

---
+++ Projects & Algorithms/Recursion/ToDo1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ToDo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sigma(2.5));
+            Console.WriteLine(Factorial(6.5));
+
+        }
+
+        public static double Sigma(double value)
+        {
+            value = Math.Floor(value);
+            if(value < 0) return 0;
+            if(value == 0) return value;
+            return value + Sigma(value - 1);
+        }
+
+        public static double Factorial(double value)
+        {
+            value = Math.Floor(value);
+            if(value < 0) return 0;
+            if(value == 0) return 1;
+            return value * Factorial(value - 1);
+        }
+    }
+}
